c2e66633e6565496fd983a3d728f3cb364275ea0
Y4nnLS <101208372+Y4nnLS@users.noreply.github.com>
2024-05-17 20:07:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture("Videos/2.mp4")
+pTime = 0
+while True:
+    sucess, img = cap.read()
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20,70),cv2.FONT_HERSHEY_PLAIN, 3,(0,255,0),3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
