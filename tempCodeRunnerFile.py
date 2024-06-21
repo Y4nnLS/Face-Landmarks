e33316ed6976a3e119f9d21a2b199833a317e1c4
Gael Huk Kukla <106789047+kyeruii@users.noreply.github.com>
2024-06-21 16:00:47 -0300

    deletei tudo

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-# Separar imagens em um dicionário por prefixo
-faces_dict = {}
-for arq in lista_faces_captured:
-    partes = arq.split('_')
-    if len(partes) >= 2:
-        prefixo = partes[0]
-        if prefixo not in faces_dict:
-            faces_dict[prefixo] = []
-        faces_dict[prefixo].append(arq)
--- utils/utils.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-def padronizar_imagem(imagem_caminho):
-    imagem = cv2.imread(imagem_caminho, cv2.IMREAD_GRAYSCALE)
-    imagem = cv2.resize(imagem, (200, 200), interpolation=cv2.INTER_LANCZOS4)
-    return imagem
